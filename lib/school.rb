a658e03674fe2d104e97ac095b3ebd8eff3c8325
Posyv <nataliepvogt@gmail.com>
2021-02-24 19:33:41 -0500

    commit tree 2864035346c077f1ac8e18489c45502f7b9d89dc

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+#  require 'pry'
+class School
+    attr_accessor :roster, :highschool
+    
+
+    def initialize(highschoolname)
+        @highschool = highschoolname
+        @roster = {}
+
+    end
+
+    def add_student(student, grade)
+        #student is a string
+        
+        if @roster[grade]
+            @roster[grade] << student
+        else 
+            # binding.pry 
+       @roster[grade] = []
+       @roster[grade] << student
+    end
+end
+
+end
+
+# ruby school = School.new("Bayside High School")
